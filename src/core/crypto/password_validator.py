6533377e332d94333a3a1b1756ae27e5966fdb7d
trivagr <sh.ego.dan@gmail.com>
2026-05-22 05:08:50 +0300

    обавлен валидатор пароля

---
+++ src/core/crypto/password_validator.py
@@ -0,0 +1,29 @@
+import re
+
+common_patterns = {
+    "password",
+    "qwerty",
+    "111111",
+    "admin",
+    "12345"
+}
+
+def validate_password(password: str) -> bool:
+
+    if len(password) < 12:
+        return False
+
+    has_upper = bool(re.search(r"[A-Z]", password))
+    has_lower = bool(re.search(r"[a-z]", password))
+    has_digit = bool(re.search(r"[0-9]", password))
+    has_special = bool(re.search(r"[!@#$%^&*()_+={};':|,.<>?~`-]", password))
+
+    if not(has_upper and has_lower and has_digit and has_special):
+        return False
+
+    lower_pass = password.lower()
+    for pattern in common_patterns:
+        if pattern in lower_pass:
+            return False
+
+    return True
